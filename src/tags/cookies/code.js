004ed7cded6884e8e4a0edc2d257ec9c31c47551
Jempe <info@jempe.org>
2024-09-30 11:49:40 -0500

    Fix environment cookie issue

--- src/tags/cookies/code.js
+++ src/tags/cookies/code.js
@@ -1,8 +1,11 @@
 // Description: Cookie Template
 // Author: Juan Castro
-// Last modified: 2023-10-24
+// Last modified: 2024-09-30
 // License: Apache 2.0
-// Version: 1.0.0
+// Version: 1.0.2
+
+// What's new 
+// Fix environment cookie issue
 
 // API imports
 const queryPermission = require('queryPermission');
@@ -15,9 +18,17 @@ const nantuModeCookieName = 'nantu_mode';
 const nantuModeQueryVariableName = 'nantu_mode';
 const nantuQAModeEventName = 'nantu_qa_mode';
 
+if(data.cookieType == "nantu_mode") {
 
-if (isNantuQAModeEvent() || hasQAQuery()) {
-	setQAModeCookie();
+  if (isNantuQAModeEvent() || hasQAQuery()) {
+	setNantuCookie("nantu_mode");
+  }
+}
+
+if(data.cookieType == "nantu_environment") {
+  if (hasEnvironmentQuery()) {
+    setNantuCookie("nantu_environment");
+  }
 }
 
 data.gtmOnSuccess();
@@ -39,7 +50,19 @@ function hasQAQuery() {
 	if (queryPermission('get_url', 'query')) {
 		const nantuModeQuery = getUrl('query');
 
-		if (nantuModeQuery.indexOf(nantuModeQueryVariableName + "=qa") !== -1) {
+		if (nantuModeQuery.indexOf("nantu_mode=qa") !== -1) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function hasEnvironmentQuery() {
+    if (queryPermission('get_url', 'query')) {
+		const nantuEnvironmentQuery = getUrl('query');
+
+		if (nantuEnvironmentQuery.indexOf("nantu_environment=qa") !== -1) {
 			return true;
 		}
 	}
@@ -48,7 +71,7 @@ function hasQAQuery() {
 }
 
 //set the QA mode cookie
-function setQAModeCookie() {
+function setNantuCookie(cookieName) {
 	// Set Cookie Permissions
 	const options = {
 		'domain': 'auto',
@@ -57,8 +80,7 @@ function setQAModeCookie() {
 		'secure': true
 	};
 
-	if (queryPermission('set_cookies', nantuModeCookieName, options)) {
-		setCookie(nantuModeCookieName, 'qa', options);
+	if (queryPermission('set_cookies', cookieName, options)) {
+		setCookie(cookieName, 'qa', options);
 	}
 }
-
